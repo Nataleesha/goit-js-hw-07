4b8ef5ea0439d2998a6f51b457b762cd21aa5bf4
Nataleesha <frojlan@gmail.com>
2023-01-16 03:23:27 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,58 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+createGallery();
+
+gallery.addEventListener("click", openImage);
+
+function createGallery() {
+  let galleryElements = "";
+
+  galleryItems.forEach((image) => {
+    const galleryItem = document.createElement("div");
+    galleryItem.classList.add("gallery__item");
+
+    const galleryLink = document.createElement("a");
+    galleryLink.classList.add("gallety__link");
+    galleryLink.href = image.original;
+    galleryItem.append(galleryLink);
+
+    const galleryImg = document.createElement("img");
+    galleryImg.classList.add("gallery__image");
+    galleryImg.src = image.preview;
+    galleryImg.dataset.source = image.original;
+    galleryImg.alt = image.description;
+    galleryLink.append(galleryImg);
+
+    galleryElements += galleryItem.innerHTML;
+  });
+
+  gallery.insertAdjacentHTML("beforeend", galleryElements);
+}
+
+function openImage(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const imageSrc = event.target.dataset.source;
+
+  const instance = basicLightbox.create(`<img src="${imageSrc}">`);
+
+  instance.show(onModalShow);
+}
+
+function onModalShow(instance) {
+  document.addEventListener("keydown", closeModal);
+
+  function closeModal(event) {
+    if (event.code === "Escape") {
+      instance.close();
+      document.removeEventListener("keydown", closeModal);
+    }
+  }
+}
